5582c81854f610b4cb6ae70d708b8239c36179c3
RAJA PREM SAI MADDULA <123068364+RajaPremSai@users.noreply.github.com>
2025-12-16 02:08:18 +0530

    schema func declared

--- cmd/cli/schema.go
+++ cmd/cli/schema.go
@@ -1,1 +1,5 @@
 package cli
+
+func fetchK8sSchema(map[string]interface{}, error)
+func fetchResourceNames(resourceName string) ([]string, error)
+func fetchSchemaForResource(resourceType string) (map[string]interface{}, error)
