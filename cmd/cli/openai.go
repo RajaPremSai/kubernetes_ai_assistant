63c087db5ec0d73e203a7183ce61090a3598c4e9
RAJA PREM SAI MADDULA <123068364+RajaPremSai@users.noreply.github.com>
2025-12-16 01:15:07 +0530

    func openaiGPTCompletion

--- cmd/cli/openai.go
+++ cmd/cli/openai.go
@@ -2,9 +2,28 @@ package cli
 
 import (
 	"context"
+	"fmt"
 	"strings"
+
+	openai "github.com/sashabaranov/go-openai"
 )
 
-func (c *oaiClients) openaiGptCompletion(ctx context.Context, prompt *strings.Builder, temp float32) (string, error)
+func (c *oaiClients) openaiGptCompletion(ctx context.Context, prompt *strings.Builder, temp float32) (string, error) {
+	req := openai.CompletionRequest{
+		Prompt:      []string{prompt.String()},
+		Echo:        false,
+		N:           1,
+		Temperature: temp,
+	}
+
+	resp, err := c.openAIClient.CreateCompletion(ctx, req)
+	if err != nil {
+		return "", err
+	}
+	if len(resp.Choices) != 1 {
+		return "", fmt.Errorf("expected choices to be 1 but received : %d", len(resp.Choices))
+	}
+	return resp.Choices[0].Text, nil
+}
 
 func (c *oaiClients) openaiGptChatCompletion(ctx context.Context, prompt *strings.Builder, temp float32) (string, error)
